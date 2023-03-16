8da220049384b81adddfb2f832c933ecf2511161
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-03-15 22:31:09 -0700

    forwardRef

--- lib/reactObserver/reactObserver.ts
+++ lib/reactObserver/reactObserver.ts
@@ -1,28 +1,30 @@
 import RenderFn from './RenderFn'
-import { FC, useRef } from 'react'
+import { forwardRef, useRef } from 'react'
 import Observable from '../Observable'
 import diff from 'set-diffs'
 import useRerender from '@utilityjs/use-force-rerender'
 
-const reactObserver = <P = {}>(renderFn: RenderFn<P>): FC<P> => (props) => {
-  const rerender = useRerender()
-  const previousObservablesRef = useRef(new Set<Observable<unknown>>())
-  const observables = new Set<Observable<unknown>>()
-  const returnValue = renderFn((observable) => {
-    observables.add(observable)
-    return observable.getValue()
-  }, props)
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const reactObserver = <Props = {}, Ref = never>(renderFn: RenderFn<Props, Ref>) =>
+  forwardRef<Ref, Props>((props, ref) => {
+    const rerender = useRerender()
+    const previousObservablesRef = useRef(new Set<Observable<unknown>>())
+    const observables = new Set<Observable<unknown>>()
+    const returnValue = renderFn((observable) => {
+      observables.add(observable)
+      return observable.getValue()
+    }, props, ref)
 
-  const { add, remove } = diff(previousObservablesRef.current, observables)
-  add.forEach(({ addRemove: { add } }) => {
-    add(rerender)
-  })
-  remove.forEach(({ addRemove: { remove } }) => {
-    remove(rerender)
-  })
-  previousObservablesRef.current = observables
+    const { add, remove } = diff(previousObservablesRef.current, observables)
+    add.forEach(({ addRemove: { add } }) => {
+      add(rerender)
+    })
+    remove.forEach(({ addRemove: { remove } }) => {
+      remove(rerender)
+    })
+    previousObservablesRef.current = observables
 
-  return returnValue
-}
+    return returnValue
+  })
 
 export default reactObserver
